4ee0a7bb92410de321bdc3687e5dbc157a752a1a
praveengak <arugilpraveen@gmail.com>
2025-03-09 17:20:50 +0530

    Project SetUp

---
+++ src/test/java/com/college/admisson/college_admission/CollegeAdmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.admisson.college_admission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeAdmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
